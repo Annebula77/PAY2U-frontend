03be83af87fff3cd7bf0721a836c4718cab6c784
goharvard77 <goharvard77@gmail.com>
2024-03-28 19:20:10 +0300

    DEV-15: add tabs logics & catalogue tab

--- src/components/homepageContent/HomePageContent.tsx
+++ src/components/homepageContent/HomePageContent.tsx
@@ -1,14 +1,26 @@
+import { Typography } from "@mui/material";
 import { StyledSection } from "../../styles/pageAndOnboardingStyles";
 import Header from "../header/Header";
 import Main from "../main/Main";
+import MainPageTabs from "../mainPageTabs/MainPageTabs";
+import styled from "styled-components";
 
-
+const TabsWrapper = styled.div`
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  `;
 
 const HomePageContent = () => {
   return (
     <StyledSection>
       <Header />
-      {/* <Main /> */}
+      <Main>
+        <Typography variant="h2" align="left" sx={{ margin: '20px 0px 20px 8px' }}>Все подписки</Typography>
+        <TabsWrapper>
+          <MainPageTabs />
+        </TabsWrapper>
+      </Main>
     </StyledSection>
 
   )
